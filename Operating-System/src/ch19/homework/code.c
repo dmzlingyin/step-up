437ff24d2c1379baf74c4d945f324a64aeacdc99
dmzlingyin <dmzlingyin@163.com>
2022-05-28 21:51:54 +0800

    remove warning

--- Operating-System/src/ch19/homework/code.c
+++ Operating-System/src/ch19/homework/code.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
 
 #define PAGESIZE 4096
 
+int mygettime();
+
 int
 main(int argc, char *argv[]) 
 {
     if (argc != 3) {
-        fprintf(2, "wrong usage.");
+        printf("wrong usage.");
         exit(1);
     }
     
-    int NUMPAGES = *argv[1];
-    int TRYS = *argv[2];
+    int NUMPAGES = *argv[1] - '0';
+    int TRYS = *argv[2] - '0';
+    printf("%s, %s\n", NUMPAGES, TRYS); 
     
     int a[NUMPAGES*PAGESIZE/sizeof(int)];
     int jump = PAGESIZE / sizeof(int);
-    printf("%s, %s\n", NUMPAGES, TRYS); 
     
     int sum = 0;
     for (int k = 0; k < TRYS; k++) {
