8e047def7e053841a4a6b31ab2fe0da493332940
dmzlingyin <dmzlingyin@163.com>
2022-06-30 21:47:15 +0800

    sleep run ok

--- Operating-System/xv6/cs6s081/sleep.c
+++ Operating-System/xv6/cs6s081/sleep.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include "kernel/types.h"
+#include "user/user.h"
 
 int 
 main(int argc, char *argv[]) 
 {
-  if (argc == 1 || argc >= 3)
-    panic("sleep usage: sleep secs");
-  return 0;
+  if (argc <= 1 || argc >= 3)
+  {
+    fprintf(2, "%s\n", "usage: sleep seconds");
+    exit(1);
+  }
+
+  int secs = atoi(argv[1]);
+  printf("sleep %ds\n", secs);
+  exit(0);
 }
 
-void
-panic(char *s) 
-{
-  fprintf(2, "%s\n", s);
-  exit(1);
-}
