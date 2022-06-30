04079b8a3012910b6e2f945aa17180c4b1da8e57
dmzlingyin <dmzlingyin@163.com>
2022-06-30 23:29:26 +0800

    sleep done

--- Operating-System/xv6/cs6s081/sleep.c
+++ Operating-System/xv6/cs6s081/sleep.c
@@ -4,14 +4,13 @@
 int 
 main(int argc, char *argv[]) 
 {
-  if (argc <= 1 || argc >= 3)
-  {
-    fprintf(2, "%s\n", "usage: sleep seconds");
+  if (argc <= 1 || argc >= 3){
+    fprintf(2, "%s\n", "Usage: sleep seconds");
     exit(1);
   }
 
   int secs = atoi(argv[1]);
-  printf("sleep %ds\n", secs);
+  sleep(secs);
   exit(0);
 }
 
