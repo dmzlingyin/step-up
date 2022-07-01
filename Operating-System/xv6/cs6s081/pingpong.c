b2fad25943d210f50ef309663d24f693fda32e8e
dmzlingyin <dmzlingyin@163.com>
2022-07-01 23:56:44 +0800

    fix sleep bug; pingpong done

--- Operating-System/xv6/cs6s081/pingpong.c
+++ Operating-System/xv6/cs6s081/pingpong.c
@@ -1,19 +1,35 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+#define BUFSIZE 4
+
+void mexit(char *s)
+{
+  fprintf(2, "%s\n", s);
+  exit(1);
+}
+
 int
 main(int argc, char *argv[])
 {
-  int c = fork();
+  int p1[2], p2[2];
+  char buf1[BUFSIZE], buf2[BUFSIZE];
 
+  if (pipe(p1) < 0 || pipe(p2) < 0)
+    mexit("pip error");
+
+  int c = fork();
   if (c == 0) {
-    printf("<%d>: child process\n", getpid());
+    read(p2[0], buf2, BUFSIZE);
+    write(p1[1], "pong", BUFSIZE);
+    printf("%d: received %s\n", getpid(), buf2);
   } else if (c > 0) {
+    write(p2[1], "ping", BUFSIZE);
     wait(0);
-    printf("<%d>: parent process\n", getpid());
+    read(p1[0], buf1, BUFSIZE);
+    printf("%d: reveived %s\n", getpid(), buf1);
   } else {
-    fprintf(2, "wrong\n");
-    exit(1);
+    mexit("fork error");
   }
 
   exit(0);
--- Operating-System/xv6/cs6s081/sleep.c
+++ Operating-System/xv6/cs6s081/sleep.c
@@ -1,23 +1,16 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-void mpanic(){
-    fprintf(2, "%s\n", "Usage: sleep seconds(positive)");
-    exit(1);
-}
-
 int 
 main(int argc, char *argv[]) 
 {
-  if (argc <= 1 || argc >= 3){
-      mpanic();
+  if (argc != 2){
+    fprintf(2, "%s\n", "Usage: sleep seconds");
+    exit(1);
   }
 
   int secs = atoi(argv[1]);
-  if (secs < 0)
-      mpanic();
   sleep(secs);
-
   exit(0);
 }
 
