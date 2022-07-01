e8ed8afe1accec50a6c7a3ec876cac7d0caafb94
dmzlingyin <dmzlingyin@163.com>
2022-07-01 16:02:09 +0800

    add condition_variable.c

--- Operating-System/xv6/cs6s081/sleep.c
+++ Operating-System/xv6/cs6s081/sleep.c
@@ -1,16 +1,23 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+void mpanic(){
+    fprintf(2, "%s\n", "Usage: sleep seconds(positive)");
+    exit(1);
+}
+
 int 
 main(int argc, char *argv[]) 
 {
   if (argc <= 1 || argc >= 3){
-    fprintf(2, "%s\n", "Usage: sleep seconds");
-    exit(1);
+      mpanic();
   }
 
   int secs = atoi(argv[1]);
+  if (secs < 0)
+      mpanic();
   sleep(secs);
+
   exit(0);
 }
 
