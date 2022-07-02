36fa22b2d6cfb3806d649f75e7c4ff125999984b
dmzlingyin <dmzlingyin@163.com>
2022-07-02 20:39:09 +0800

    add pcc(producer consumer condition)

--- Operating-System/src/ch30/pcc_v2.c
+++ Operating-System/src/ch30/pcc_v2.c
@@ -8,7 +8,8 @@ int loops = 100;
 
 // lock, condition 初始化
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
 
 void
 put(int value) 
@@ -32,12 +33,13 @@ void
     int i;
     for (i = 0; i < loops; i++){
         Pthread_mutex_lock(&lock);        
-        if(count == 1)
-            pthread_cond_wait(&cond, &lock);
+        while (count == 1)
+            pthread_cond_wait(&empty, &lock);
         put(i);
-        pthread_cond_signal(&cond);
+        pthread_cond_signal(&fill);
         Pthread_mutex_unlock(&lock);
     }
+    return NULL;
 }
 
 void
@@ -46,22 +48,26 @@ void
     int i;
     for(i = 0; i < loops; i++){
         Pthread_mutex_lock(&lock);
-        if(count == 0)
-            pthread_cond_wait(&cond, &lock);
+        while (count == 0)
+            pthread_cond_wait(&fill, &lock);
         int tmp = get();
-        pthread_cond_signal(&cond);
+        pthread_cond_signal(&empty);
         Pthread_mutex_unlock(&lock);
         printf("%d\n", tmp);
     }
+    return NULL;
 }
 
 int 
 main(int argc, char *argv[])
 {
-    pthread_t p1, p2;
+    pthread_t p1, p2, p3;
     Pthread_create(&p1, NULL, producer, NULL);
     Pthread_create(&p2, NULL, consumer, NULL);
+    Pthread_create(&p3, NULL, consumer, NULL);
     Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
+    Pthread_join(p3, NULL);
+
     return 0;
 }
