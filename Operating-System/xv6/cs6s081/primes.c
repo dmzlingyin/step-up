0b4b486ac91f3ffefa03960624cd419cf97158cd
dmzlingyin <dmzlingyin@163.com>
2022-07-05 00:16:06 +0800

    add function declare

--- Operating-System/xv6/cs6s081/primes.c
+++ Operating-System/xv6/cs6s081/primes.c
@@ -5,12 +5,19 @@
 质数筛详情:https://swtch.com/~rsc/thread/
  */
 
+int generateNatural();
+int primeFilter(int in_fd, int prime);
 int
 main(int argc, char *argv[])
 {
+    int fd[2];
+    pipe(fd);
+
     int c = fork();
     if (c > 0) {
-        printf("%d\n", getpid());
+        for (int i = 2; i <= 35; i++) {
+
+        }
     } else if (c == 0) {
         printf("%d\n", getpid());
     } else {
