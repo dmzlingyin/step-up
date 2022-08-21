b4723e62452564faa4c508de17a5b57376d3a07f
Lv Wenchao <dmzlingyin@163.com>
2022-08-21 17:52:33 +0800

    add producer_consumer

---
+++ Golang/code/producer_consumer.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Producer(factor int, out chan<- int) {
+	i := 0
+	for {
+		out <- i * factor
+		i++
+	}
+}
+
+func Consumer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	ch := make(chan int, 64)
+
+	go Producer(3, ch)
+	go Producer(5, ch)
+	go Consumer(ch)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Printf("quit (%v)\n", <-sig)
+}
