2d727edd5cc616a6ceab2b92c8415d4d1336042f
dmzlingyin <dmzlingyin@163.com>
2022-09-20 20:35:46 +0800

    add sleep.c

---
+++ Operating-System/xv6-riscv/user/sleep.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int 
+main(int argc, char *argv[]) {
+    int secs;
+    if (argc < 2) {
+        fprintf(2, "Usage: sleep seconds\n");
+        exit(1);
+    }
+
+    secs = atoi(argv[1]);
+    if (secs < 0) {
+        fprintf(2, "Param wrong\n");
+        exit(1);
+    }
+
+    sleep(secs); 
+    exit(0);
+}
