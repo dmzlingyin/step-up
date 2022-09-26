1015bc2742826439b0b76cce4ca905a6e73bbdad
dmzlingyin <dmzlingyin@163.com>
2022-09-26 19:58:05 +0800

    add nil channel

---
+++ Golang/code/channel/nil_channel.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1, c2 := make(chan int), make(chan int)
+
+	go func() {
+		time.Sleep(time.Second * 5)
+		c1 <- 5
+		close(c1)
+	}()
+	go func() {
+		time.Sleep(time.Second * 7)
+		c2 <- 7
+		close(c2)
+	}()
+
+	for {
+		select {
+		case x, ok := <-c1:
+			if !ok {
+				c1 = nil
+			} else {
+				fmt.Println(x)
+			}
+		case y, ok := <-c2:
+			if !ok {
+				c2 = nil
+			} else {
+				fmt.Println(y)
+			}
+		}
+		if c1 == nil && c2 == nil {
+			break
+		}
+	}
+
+	fmt.Println("program done.")
+}
